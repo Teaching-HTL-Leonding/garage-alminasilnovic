3884c241b1f6cd3c1e599ce4df8c589ac51cc4c6
Almina Silnovic <alminasilnovic10@gmail.com>
2023-11-16 18:37:04 +0100

    Garage

---
+++ Garage.App/Program.cs
@@ -0,0 +1,67 @@
+﻿using Garage.Logic;
+
+int parkingSpotNumber = 0;
+string licensePlate = "";
+DateTime entryDate;
+DateTime exitDate;
+decimal costs = 0;
+string selection = "";
+Garage.Logic.Garage garage = new Garage.Logic.Garage();
+
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("1) Enter a car entry");
+Console.WriteLine("2) Enter a car exit");
+Console.WriteLine("3) Generate report");
+Console.WriteLine("4) Exit");
+Console.WriteLine();
+
+
+do
+{
+    Console.Write("Your selection: ");
+    selection = Console.ReadLine()!;
+    Console.WriteLine();
+
+    switch (selection)
+    {
+        case "1":
+            Console.Write("Enter parking spot number: ");
+            parkingSpotNumber = int.Parse(Console.ReadLine()!);
+            if (garage.IsOccupied(parkingSpotNumber))
+            {
+                Console.WriteLine("Parking spot is occupied");
+            }
+            else
+            {
+                Console.Write("Enter license plate: ");
+                licensePlate = Console.ReadLine()!;
+                Console.Write("Enter entry date: ");
+                entryDate = DateTime.Parse(Console.ReadLine()!);
+                garage.TryOccupy(parkingSpotNumber, licensePlate, entryDate);
+            }
+            break;
+        case "2":
+            Console.Write("Enter parking spot number: ");
+            parkingSpotNumber = int.Parse(Console.ReadLine()!);
+            if (!garage.IsOccupied(parkingSpotNumber))
+            {
+                Console.WriteLine("Parking spot is not occupied");
+            }
+            else
+            {
+                Console.Write("Enter exit date: ");
+                exitDate = DateTime.Parse(Console.ReadLine()!);
+                garage.TryExit(parkingSpotNumber, exitDate, out costs);
+            }
+            break;
+        case "3":
+            garage.GenerateReport();
+            break;
+        case "4":
+            Console.WriteLine("Good bye!");
+            break;
+        default:
+            return;
+    }
+}
+while (selection != "4");
---
+++ Garage.Logic/Garage.cs
@@ -0,0 +1,70 @@
+﻿namespace Garage.Logic;
+public class ParkingSpot
+{
+    public ParkingSpot(string licensePlate, DateTime entryDate)
+    {
+        LicensePlate = licensePlate;
+        EntryDate = entryDate;
+    }
+
+    public string LicensePlate { get; set; } = "";
+    public DateTime EntryDate { get; set; }
+}
+
+public class Garage
+{
+    public ParkingSpot[] parkingSpots { get; } = new ParkingSpot[50];
+
+    public bool IsOccupied(int parkingSpotNumber)
+    {
+        if (parkingSpots[parkingSpotNumber - 1] != null)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public bool TryOccupy(int parkingSpotNumber, string licensePlate, DateTime entryTime)
+    {
+        if (IsOccupied(parkingSpotNumber) == true)
+        {
+            return false;
+        }
+        parkingSpots[parkingSpotNumber - 1] = new ParkingSpot(licensePlate, entryTime);
+        return true;
+    }
+
+    public bool TryExit(int parkingSpotNumber, DateTime exitTime, out decimal costs)
+    {
+        if (IsOccupied(parkingSpotNumber))
+        {
+            decimal startedHalfHour = (decimal)Math.Ceiling(((exitTime - parkingSpots[parkingSpotNumber - 1].EntryDate).TotalMinutes) / 30);
+            costs = startedHalfHour * 3;
+            Console.WriteLine($"Costs are {costs}$");
+            parkingSpots[parkingSpotNumber - 1] = null!;
+            return true;
+        }
+        costs = 0;
+        return false;
+    }
+
+    public void GenerateReport()
+    {
+        Console.WriteLine("| Spot | License Plate |");
+        Console.WriteLine("|------|---------------|");
+
+        for (int i = 0; i < parkingSpots.Length; i++)
+        {
+            ParkingSpot spot = parkingSpots[i];
+
+            if (spot != null)
+            {
+                Console.WriteLine($"| {i + 1,-4} | {spot.LicensePlate,-13} |");
+            }
+            else
+            {
+                Console.WriteLine($"| {i + 1,-4} |               |");
+            }
+        }
+    }
+}
